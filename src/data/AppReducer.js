783df85712bf6f5348a2b0bf21054284396da343
MrKazio <115210775+Dzban0@users.noreply.github.com>
2024-11-05 13:26:09 +0100

    Lab4

--- src/data/AppReducer.js
+++ src/data/AppReducer.js
@@ -1,19 +1,23 @@
-export default function AppReducer(state, action){
-    switch(action.type){
-        case "edit":{
-                //edit function
-            return state;
-        }
-        case "rate":{
-            
-            return state.map(item=>
-                item.id===action.id?{...item,rating:action.newRating}:item
+export default function AppReducer(state, action) {
+    switch (action.type) {
+        case 'add':
+           return [...state, { id: state.length + 1, ...action.newItem }];
+        case "edit": {
+            return state.map(item =>
+                item.id === action.id ? { ...item, ...action.updatedData } : item
             );
         }
-        case "delete":{
-            return state.filter(item=>item.id!==action.id); 
-        }  
+        case "rate": {
+            return state.map(item =>
+                item.id === action.id
+                    ? { ...item, rating: action.newRating }
+                    : item
+            );
+        }
+        case "delete": {
+            return state.filter(item => item.id !== action.id);
+        }
         default:
-            return state;    
+            return state;
     }
 }
